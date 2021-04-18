9536e758bd29f884a67c6dabbd143274741c81d8
Nischal Newar <nischalnewar009@gmail.com>
2021-04-18 16:45:52 +0530

    Added Matka scene and it's related models and prefab

---
+++ VR Mela/Assets/VR Mela/Scripts/removePieces.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class removePieces : MonoBehaviour
+{
+    void Update(){
+        Destroy(gameObject, 2.0f);
+    }
+}
